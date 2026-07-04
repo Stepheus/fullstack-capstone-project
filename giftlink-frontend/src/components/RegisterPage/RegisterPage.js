14d64da5f880801028e49716ed9e9e9fe28d0a21
Stepheus <stephen.iboudah@outlook.com>
2026-07-03 20:06:25 -0500

    minor edits on frontEnd, Register page

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -20,27 +20,34 @@ function RegisterPage(){
                 <div className="col-md-6 col-lg-4">
                     <div className="register-card p-4 border rounded">
                         <h2 className="text-center mb-4 font-weight-bold">Register</h2>
-                        <div className="mb-4">
+                        <div className="mb-3">
                             <label htmlFor="firstName" className="form-label">Firstname</label>
                             <input id="firstName" type="text" className="form-control" placeholder="Enter your firstname" value={firstName}
                             onChange={(e)=> setFirstName(e.target.value)}/>
-
-                            <label htmlFor="lastName" className="form-label">Lastname</label>
-                            <input id="lastName" type="text" className="form-control" placeholder="Enter your lastname" value={lasttName}
-                            onChange={(e)=> setLastName(e.target.value)}/>
-
-                            <label htmlFor="email" className="form-label">email</label>
-                            <input id="email" type="email" className="form-control" placeholder="Enter your email" value={email}
-                            onChange={(e)=> setEmail(e.target.value)}/>
-
-                            <label htmlFor="password" className="form-label">Password</label>
-                            <input id="password" type="text" className="form-control" placeholder="Enter your password" value={password}
-                            onChange={(e)=> setPassword(e.target.value)}/>
-
-
-                            <button className="bttn bttn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
                         </div>
 
+
+                         <div className="mb-3">
+                               <label htmlFor="lastName" className="form-label">Lastname</label>
+                            <input id="lastName" type="text" className="form-control" placeholder="Enter your lastname" value={lastName}
+                            onChange={(e)=> setLastName(e.target.value)}/>
+                         
+                        </div>
+
+                         <div className="mb-3">
+                                  <label htmlFor="email" className="form-label">Email</label>
+                            <input id="email" type="email" className="form-control" placeholder="Enter your email" value={email}
+                            onChange={(e)=> setEmail(e.target.value)}/>
+                        </div>
+
+
+                         <div className="mb-3">
+                             <label htmlFor="password" className="form-label">Password</label>
+                            <input id="password" type="password" className="form-control" placeholder="Enter your password" value={password}
+                            onChange={(e)=> setPassword(e.target.value)}/>                      
+                        </div>              
+
+                            <button className="bttn bttn-primary w-100 mb-3" onClick={handleRegister}>Register</button>
 
                         <p className="mt-4 text-center"> 
                             Already a member? <a href="/app/login" className="text-primary">Login</a>
